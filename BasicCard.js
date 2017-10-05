99329cb44816f77431513862c7e5f01aaf6e7091
bhill7473 <bhill7473@gmail.com>
2017-10-05 17:15:04 -0400

    homework

--- BasicCard.js
+++ BasicCard.js
@@ -1,83 +1,18 @@
 module.exports = BasicCard;
 var inquirer=require("inquirer");
+var fs = require("fs");
 function  BasicCard(front,back){
 	this.front=front;
 	this.back=back;
-}
 
-var questionOne = new BasicCard("What was the first state?","Delaware");
-var questionTwo = new BasicCard("The only state covered by it own time zone is?","Hawaii");
-var questionThree = new BasicCard("What state is located furthest west?","Alaska");
-var questionFour = new BasicCard("What state is known for their cheese?","Wisconsin");
-var questionFive = new BasicCard("What state has a cardinal for their state bird?","Ohio");
-var questionSix = new BasicCard("What state is known as the sunshine state?","Florida");
+  if(error){
+    console.log(error);
+  }
+ var questionOne = new BasicCard(this.front,this.back);
+ 
+};
 
-inquirer
-  .prompt([
-  
-    {
-      type: "input",
-      message: questionOne.front,
-      name: "q1"
-    },
-     {
-      type: "answer",
-      message: questionOne.front + " Correct answer is: " + questionOne.back,
-      name: "q1"
-    },
-    {
-      type: "input",
-      message: questionTwo.front,
-      name: "q2"
-     
-    },
-     {
-      type: "answer",
-      message: questionTwo.front + " Correct answer is: " + questionTwo.back,
-      name: "q1"
-    },
-     {
-      type: "input",
-      message: questionThree.front,
-      name: "q3"
-     
-    },
-     {
-      type: "answer",
-      message: questionThree.front + " Correct answer is: " + questionThree.back,
-      name: "q1"
-    },
-    {
-      type: "input",
-      message: questionFour.front,
-      name: "q4"
-    },
-     {
-      type: "answer",
-      message: questionFour.front + " Correct answer is: " + questionFour.back,
-      name: "q1"
-    },
-    {
-      type: "input",
-      message: questionFive.front,
-      name: "q5"
-     
-    },
-     {
-      type: "answer",
-      message: questionFive.front + " Correct answer is: " + questionFive.back,
-      name: "q1"
-    },
-     {
-      type: "input",
-      message: questionSix.front,
-      name: "q6"
-     
-    },
-     {
-      type: "answer",
-      message: questionSix.front + " Correct answer is: " + questionSix.back,
-      name: "q1"
-    },
-])
+
+
+
   
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,3 +1,4 @@
+var basicard = require("./basiccard.js");
 module.exports=ClozeCard;
 var inquirer = require("inquirer");
 var fs = require("fs");
@@ -7,81 +8,10 @@ function ClozeCard(text,partial,cloze){
 	this.partial=partial;
 	this.cloze=cloze;
 	
-	
+  if(error){
+    console.log(error);
+  }
+	var questionOne = new ClozeCard(this.text + this.partial,this.cloze);
 };
 
-var questionOne = new ClozeCard("Fort Knox underground vault filled with gold bars is located in", "......","Kentucky");
-var questionTwo = new ClozeCard("The capital of Ohio is", ".....","Columbus");
-var questionThree = new ClozeCard("The Mexican city of Tijuana borders", "......","San Diego");
-var questionFour = new ClozeCard("The longest coastline in the U.S., 6,640 miles, greater than that of all other states combined is in", ".....","Alaska");
-var questionFive = new ClozeCard("The only active diamond mine in the U.S. is located in", "......","Arkansas");
-var questionSix = new ClozeCard("The world's largest silver nugget (1,840 pounds) found in 1894 near Aspen", ".....","Colorado");
-inquirer
-  .prompt([
-  
-    {
-      type: "input",
-      message: questionOne.text + questionOne.partial,
-      name: "q1"
-    },
-    {
-    	type: "answer",
-    	message: questionOne.text + " " + questionOne.cloze + "." + " Is the correct answer",
-    	name: "q1"
-    },
-    {
-      type: "input",
-      message: questionTwo.text + questionTwo.partial,
-      name: "q2"
-     
-    },
-     {
-    	type: "answer",
-    	message: questionTwo.text + " " + questionTwo.cloze + "." + " Is the correct answer",
-    	name: "q1"
-    },
-     {
-      type: "input",
-      message: questionThree.text + questionThree.partial,
-      name: "q3"
-    },
-     {
-    	type: "answer",
-    	message: questionThree.text + " " + questionThree.cloze + "." + " Is the correct answer",
-    	name: "q1"
-    },
-    {
-      type: "input",
-      message: questionFour.text + questionFour.partial,
-      name: "q4"
-     
-    },
-     {
-    	type: "answer",
-    	message: questionFour.text + " " + questionFour.cloze + "." + " Is the correct answer",
-    	name: "q1"
-    },
-     {
-      type: "input",
-      message: questionFive.text + questionFive.partial,
-      name: "q5"
-    },
-     {
-    	type: "answer",
-    	message: questionFive.text + " " + questionFive.cloze + "." + " Is the correct answer",
-    	name: "q1"
-    },
-    {
-      type: "input",
-      message: questionSix.text + questionSix.partial,
-      name: "q6"
-     
-    },
-     {
-    	type: "answer",
-    	message: questionSix.text + " " + questionSix.cloze + "." + " Is the correct answer",
-    	name: "q1"
-    },
-   
-  ])
- 
+
